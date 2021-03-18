5063425701595fc92cdaaba6a7face02c1a3bedf
William Kray <william@kray.pw>
2021-03-18 11:26:52 -0700

    use some pandoc args for formatting:   * custom build of docker container which includes chosen font   * use DroidSans font   * links are blue

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-docker run --rm -v "${PWD}":/data pandoc/latex \
+docker run --rm -v "${PWD}":/data williamkray/latex:latest \
+  --pdf-engine=xelatex \
   -V geometry:margin=0.75in \
-  -f markdown \
+  -V linkcolor:'blue' \
+  -V mainfont:"DroidSans" \
+  -f gfm \
   -t latex \
   -o "William Kray Resume.pdf" \
   README.md
